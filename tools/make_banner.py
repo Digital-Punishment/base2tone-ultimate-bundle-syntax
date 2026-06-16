3127d120466f54cdabd6d7cafd4b2ac40af93881
alopex <alopex.fduch.org@gmail.com>
2026-06-16 20:56:01 +0300

    banner for 1.1.0

--- tools/make_banner.py
+++ tools/make_banner.py
@@ -1,4 +1,4 @@
-from pathlib import Path #, PurePath
+from pathlib import Path  # , PurePath
 from PIL import Image, ImageColor, ImageDraw, ImageFont, ImageFilter
 
 import argparse
@@ -6,12 +6,7 @@ import json
 import re
 import semantic_version
 
-# WARNING
-# use default font for banner!
-# default:
-# Menlo, Consolas, DejaVu Sans Mono, monospace
-# usual:
-# Cartograph CF ML Ligatured CCG, Cascadia Code, Ubuntu Mono
+# [WARNING] use default font for banner sources!
 
 settings_path = "../lib/settings.json"
 package_path = "../package.json"
@@ -30,36 +25,35 @@ def create_banner(title: str, version: str, count: int) -> Image:
     final_size = (banner_width, int(crop_size[1] * (banner_width / crop_size[0])))
     result = Image.new("RGB", crop_size, 0)
 
-    crop = ( \
-        crop_topleft[0], \
-        crop_topleft[1], \
-        crop_topleft[0] + crop_size[0], \
-        crop_topleft[1] + crop_size[1] \
-        )
+    crop = (
+        crop_topleft[0],
+        crop_topleft[1],
+        crop_topleft[0] + crop_size[0],
+        crop_topleft[1] + crop_size[1],
+    )
 
-    screenshots = sorted(Path(screenshots_path).glob("*.png"))#[:10]
-    mask_size = ([i * 4 for i in crop_size])
+    screenshots = sorted(Path(screenshots_path).glob("*.png"))  # [:10]
+    mask_size = [i * 4 for i in crop_size]
     mask_ratio = 1.5
     for index, screenshot in enumerate(screenshots):
         print(f"Processing: {screenshot}")
         img = Image.open(screenshot)
         cropped = img.crop(crop)
 
-        mask = Image.new("L", mask_size, 255) # White background
+        mask = Image.new("L", mask_size, 255)  # White background
         draw = ImageDraw.Draw(mask)
         for y in range(mask_size[1]):
             for x in range(mask_size[0]):
                 mask_width = (
-                    ((mask_size[0] + (mask_size[1] * mask_ratio)) / len(screenshots)) *
-                    index
-                )
+                    (mask_size[0] + (mask_size[1] * mask_ratio)) / len(screenshots)
+                ) * index
                 if (x + mask_ratio * (mask_size[1] - y)) < mask_width:
-                    mask.putpixel((x, y), 0) # Black pixel
+                    mask.putpixel((x, y), 0)  # Black pixel
 
         mask = mask.resize(crop_size, Image.Resampling.LANCZOS)
 
         shadow = Image.new("L", crop_size, 0)
-        shadowmask = mask.copy().filter(ImageFilter.GaussianBlur(radius = 2.5))
+        shadowmask = mask.copy().filter(ImageFilter.GaussianBlur(radius=2.5))
 
         result.paste(shadow, (0, 0), shadowmask)
         result.paste(cropped, (0, 0), mask)
@@ -89,32 +83,32 @@ def create_banner(title: str, version: str, count: int) -> Image:
     rect_radius = 10
 
     rect_title = font_title.getbbox(text_title)
-    rect_title_off = (\
-        rect_title[0] + hoffset_title - rect_radius,\
-        rect_title[1] + voffset_title - rect_radius,\
-        rect_title[2] + hoffset_title + rect_radius,\
-        rect_title[3] + voffset_title + rect_radius\
+    rect_title_off = (
+        rect_title[0] + hoffset_title - rect_radius,
+        rect_title[1] + voffset_title - rect_radius,
+        rect_title[2] + hoffset_title + rect_radius,
+        rect_title[3] + voffset_title + rect_radius,
     )
     rect_version = font_version.getbbox(text_version)
-    rect_version_off = (\
-        rect_version[0] + hoffset_version - rect_radius,\
-        rect_version[1] + voffset_version - rect_radius,\
-        rect_version[2] + hoffset_version + rect_radius,\
-        rect_version[3] + voffset_version + rect_radius\
+    rect_version_off = (
+        rect_version[0] + hoffset_version - rect_radius,
+        rect_version[1] + voffset_version - rect_radius,
+        rect_version[2] + hoffset_version + rect_radius,
+        rect_version[3] + voffset_version + rect_radius,
     )
     rect_count = font_version.getbbox(text_count)
-    rect_count_off = (\
-        rect_count[0] + hoffset_count - rect_radius,\
-        rect_count[1] + voffset_count - rect_radius,\
-        rect_count[2] + hoffset_count + rect_radius,\
-        rect_count[3] + voffset_count + rect_radius\
+    rect_count_off = (
+        rect_count[0] + hoffset_count - rect_radius,
+        rect_count[1] + voffset_count - rect_radius,
+        rect_count[2] + hoffset_count + rect_radius,
+        rect_count[3] + voffset_count + rect_radius,
     )
 
-    draw.rounded_rectangle(rect_title_off, radius = rect_radius, fill=shadow_fill)
-    draw.rounded_rectangle(rect_version_off, radius = rect_radius, fill=shadow_fill)
-    draw.rounded_rectangle(rect_count_off, radius = rect_radius, fill=shadow_fill)
+    draw.rounded_rectangle(rect_title_off, radius=rect_radius, fill=shadow_fill)
+    draw.rounded_rectangle(rect_version_off, radius=rect_radius, fill=shadow_fill)
+    draw.rounded_rectangle(rect_count_off, radius=rect_radius, fill=shadow_fill)
 
-    shadows = shadows.filter(ImageFilter.GaussianBlur(radius = 15))
+    shadows = shadows.filter(ImageFilter.GaussianBlur(radius=15))
 
     overlay = Image.new("RGBA", crop_size, (0, 0, 0, 0))
     draw = ImageDraw.Draw(overlay)
@@ -144,23 +138,24 @@ def create_banner(title: str, version: str, count: int) -> Image:
         font=font_version,
     )
 
-    result.paste(shadows, (0, 0), mask = shadows)
-    result.paste(overlay, (0, 0), mask = overlay)
+    result.paste(shadows, (0, 0), mask=shadows)
+    result.paste(overlay, (0, 0), mask=overlay)
 
     return result.resize(final_size, Image.Resampling.LANCZOS)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Combine screenshots into banner image and adds package name, "
         "version and amount of schemes.",
     )
     parser.add_argument(
-        "-v", "--version",
-        default = "",
-        help = "Version of the package to add to a banner. It can be a version string "
-               "in SemVer format (x.y.z) or a string describing next release (major, "
-               "minor or patch), in such case version will be bumped automatically.",
+        "-v",
+        "--version",
+        default="",
+        help="Version of the package to add to a banner. It can be a version string "
+        "in SemVer format (x.y.z) or a string describing next release (major, "
+        "minor or patch), in such case version will be bumped automatically.",
     )
     args = parser.parse_args()
 
@@ -168,12 +163,18 @@ if __name__ == "__main__":
     scheme_count = 0
     next_ver = "0.0.1"
 
-    with Path(settings_path).open(mode = "r") as settings_file:
+    with Path(settings_path).open(mode="r") as settings_file:
         settings_content = json.load(settings_file)
-    with Path(package_path).open(mode = "r") as package_file:
+    with Path(package_path).open(mode="r") as package_file:
         package_content = json.load(package_file)
 
-    package_name = package_content["name"].removesuffix("-syntax").replace("-", " ").title()
+    package_name = (
+        package_content["name"]
+        .removesuffix("-tools")
+        .removesuffix("-syntax")
+        .replace("-", " ")
+        .title()
+    )
     scheme_count = len(settings_content["config"]["scheme"]["enum"])
 
     if args.version != "":
@@ -197,7 +198,7 @@ if __name__ == "__main__":
     else:
         print("Please, provide a version. See 'make_banner.py -h' for instructions.")
 
-    print(package_name, next_ver, scheme_count)
+    # print(package_name, next_ver, scheme_count)
 
     banner = create_banner(package_name, next_ver, scheme_count)
     # banner.show()
